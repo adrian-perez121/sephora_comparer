7bb34053c9c88c7ee651c08dec99aed1c879c4e2
Adrian Perez <appperez@ucdavis.edu>
2025-04-14 21:30:53 -0700

    Add functionality for the foundation of the api

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,52 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function Button({text, onClickFunc}) {
-    return <button className="bg-red-500" onClick={onClickFunc}>{text}</button>
-}
 
 function App() {
-    const [count, setCount] = useState(0)
-    const goToGithub = () => {window.location.href = "https://github.com/"}
+    const [itemStr, setItemStr] = useState('');
+    const [itemsData, setItemsData] = useState('');
+
+    const handleChange = (event) => {
+        setItemStr(event.target.value)
+    };
+
+
+    const handleEnter = (event) => {
+        if (event.key === 'Enter') {
+            const options = {
+                method: 'GET',
+                headers: {
+                    'x-rapidapi-key': import.meta.env.VITE_RAPID_API_KEY,
+                    'x-rapidapi-host': 'sephora.p.rapidapi.com'
+                }
+            };
+
+            setItemsData('');
+
+            async function getProduct() {
+                const url = `https://sephora.p.rapidapi.com/us/products/v2/search?pageSize=10&currentPage=1&q=${event.target.value}`
+                try {
+                    const response = await fetch(url, options);
+                    const result = await response.text();
+                    setItemsData(result);
+                    // Make sure the previous data is cleared first
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+
+            getProduct()
+        }
+    }
 
   return (
     <>
-      <div>
-          <Button onClickFunc={goToGithub} text={"Click Me"}/>
-      </div>
-      <p className="read-the-docs text-amber-50">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div>
+              <input className="bg-amber-50 border-amber-400 border-2 outline-none" type="text" value={itemStr} onChange={handleChange} onKeyDown={handleEnter} />
+        </div>
+        {/*  This is going to be there the cards go*/}
+        <p>{itemsData}</p>
+
     </>
   )
 }
